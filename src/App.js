92925c3cef7ec66d53626cf768382414321a3570
Parmpreet1 <Parmpreet1@github.com>
2022-08-20 22:03:59 +0530

    winner calculate 2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Board } from "./components/board";
 import "./components/root.scss";
 import { calculateWinner } from "./helpers";
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext,setisXNext]=useState(true);
+  const [isXNext, setisXNext] = useState(true);
   useEffect(() => {
     console.log(board);
-  }, [board])
-  const winner=calculateWinner(board);
-  const message=winner? `Winner is ${winner}` : `Next Player is ${isXNext? "X":"O"}`;
-  const handleOnClick=(position)=>{
-    
+  }, [board]);
+  const winner = calculateWinner(board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${isXNext ? "X" : "O"}`;
+  const handleOnClick = (position) => {
     if (board[position] || winner) {
       return;
     }
-    setBoard((prev)=>{
-      return prev.map((square,pos)=>{
-        if (pos===position) {
-          return isXNext? "X":"O";
+    setBoard((prev) => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? "X" : "O";
         }
         return square;
-      })
+      });
     });
-    setisXNext((prev)=>!prev)
-    
+    setisXNext((prev) => !prev);
   };
-  
+
   return (
     <div className="app">
-    <h1>TIC TAC TOE</h1>
-    <h2>{message}</h2>
-    <Board board={board} handleOnClick={handleOnClick}/>
-  </div>
-  )
-}
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={board} handleOnClick={handleOnClick} />
+    </div>
+  );
+};
 
 export default App;
