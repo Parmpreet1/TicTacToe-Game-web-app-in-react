3ac990653a8fea7ed8a39bc249f4115865e33246
Parmpreet1 <Parmpreet1@github.com>
2022-08-22 00:00:17 +0530

    style the entire App

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -4,17 +4,17 @@ export const StatusMessage = ({winner,current}) => {
     const noElementLeft=current.board.every(eve=>eve!==null)
   return (
     <div className='status-message'>
-          {winner && <>`Winner is <span className={winner==='X'? 'text-green' : 'text-orange'}>{winner}</span>` </>}
+          {winner && <>Winner is <span className={winner==='X'? 'text-green' : 'text-orange'}>{winner}</span> </>}
           {!winner && !noElementLeft &&
           <>
-          `Next Player is <span className={current.isXNext ? 'text-green' : 'text-orange'}>{current.isXNext ? "X" : "O"}`</span>
+          Next Player is <span className={current.isXNext ? 'text-green' : 'text-orange'}>{current.isXNext ? "X" : "O"}</span>
           </>
           }
           {!winner && noElementLeft && <>
-          <span className='text-green'>'X'</span>
-          "and"
-          <span className='text-green'>'Y'</span>
-          `is tied`
+          <span className='text-green'>X </span>
+          and
+          <span className='text-green'> Y </span>
+          is tied
           </>
           }
 
